c7b390f5f34872c94b5f5653a6253e0962f828bb
theco12 <107103821+theco12@users.noreply.github.com>
2022-09-20 19:02:23 +0900

    write.html

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express =require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
 
     console.log('8080 port')
 });
 
-app.get('/pet', function(요청, 응답){
+app.get('/pet', function(req, res){
+    res.send('반갑습니다');
+});
 
-    응답.send('반갑습니다');
+app.get('/beauty', function(req, res){
+    res.send('뷰티페이지입니다.');
+});
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add' , function(req, res){
+    res.send('전송완료')
 });
