0d9c7630f262231d5353a68c93ee137585730efa
theco12 <107103821+theco12@users.noreply.github.com>
2022-09-24 09:24:17 +0900

    React

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express =require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
+
+
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // var db;//변수를 만들어줌 
