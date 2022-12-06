603709123ea4f27dff92ea0b657508aab6f4bab6
theco12 <107103821+theco12@users.noreply.github.com>
2022-12-06 18:22:53 +0900

    update

---
+++ blog/pages/post-name/first-post.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+
+export default function FirstPosts() {
+  return (
+    <Layout>
+      <Head>
+        <title>First-Page</title>
+      </Head>
+      <h1> 첫번째 글</h1>
+    </Layout>
+  );
+}
