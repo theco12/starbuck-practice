d78e246850e532d7b423cd3d3b3a418c2779c4f7
theco12 <107103821+theco12@users.noreply.github.com>
2022-12-06 18:33:23 +0900

    update

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -32,21 +32,21 @@ export default function Home({ allPostsData }) {
           <br />
           Next.js blog trainning test
         </p>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
     </div>
   );
 }
