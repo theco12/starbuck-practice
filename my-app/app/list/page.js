a681347b763c23c6b1647ddcce8717329e06d934
theco12 <107103821+theco12@users.noreply.github.com>
2023-05-26 09:27:43 +0900

    dbupdate

--- my-app/app/list/page.js
+++ my-app/app/list/page.js
@@ -1,46 +1,22 @@
-"use client";
+import { MongoClient } from "mongodb";
+import { connectDB } from "@/util/database";
 
-import { useState } from "react";
-
-export default function List() {
-  let Gift = [
-    { name: "Tomatoes", price: 2500 },
-    { name: "Pasta", price: 4500 },
-    { name: "Coconut", price: 3500 },
-  ];
-
-  const [count, setCount] = useState(0);
+export default async function List() {
+  const client = await connectDB;
+  const db = client.db("todoapp");
+  let data = await db.collection("post").find().toArray();
+  console.log(data);
 
   return (
-    <div>
-      <h1>상품목록</h1>
-
-      {Gift.map((a, i) => {
+    <div className="list-bg">
+      {data.map((a, i) => {
         return (
-          <div key={i}>
-            <img src={`/images/food${i}.png`} width="100px" />
-            <h3>{a.name}</h3>
-            <p>{a.price}</p>
-            <button
-              onClick={() => {
-                setCount(count - 1);
-              }}>
-              -
-            </button>
-            <span>{count}</span>
-            <button
-              onClick={() => {
-                setCount(count + 1);
-              }}>
-              +
-            </button>
-
-            <hr />
+          <div key={i} className="list-item">
+            <h4>{a.title}</h4>
+            <p>{a.content}</p>
           </div>
         );
       })}
     </div>
   );
 }
-
-// styled
